944e6a6655d9f16cd7f298dea0fe59784e1a3d70
stukkki <>
2021-11-25 07:57:11 +0000

    KZaLefski

--- src/exercise7/RecursivePrimePrinter.java
+++ src/exercise7/RecursivePrimePrinter.java
@@ -5,19 +5,29 @@ import java.util.Scanner;
 public class RecursivePrimePrinter {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         System.out.print("Enter n: ");
         int n = input.nextInt();
-
         printPrimes(n);
     }
 
     public static void printPrimes(int n) {
-        // Implement recursive method
+        if (n == 0) {
+          return;
+        }
+        if (isPrime(n)) {
+          System.out.println(n);
+        }
+        printPrimes(n - 1);
     }
-
+    
     public static boolean isPrime(int n) {
-        // Implement method
-        return false;
+        int divider = 2;
+        while (divider < n) {
+          if (n % divider == 0) {
+            return false;
+          }
+          divider++;
+        }
+        return true;
     }
 }
--- src/exercise7/TimeValidation.java
+++ src/exercise7/TimeValidation.java
@@ -8,23 +8,31 @@ public class TimeValidation {
 
         System.out.print("Enter hours: ");
         int hours = input.nextInt();
-
+        boolean timeValid = false;
+        timeValid = areHoursValid(hours); 
         System.out.print("Enter minutes: ");
         int minutes = input.nextInt();
-
-        boolean timeValid = false; // Call validation methods
+          if (timeValid) {
+          timeValid = areMinutesValid(minutes);
+        } 
         System.out.println("Time valid: " + timeValid);
     }
 
-
     public static boolean areHoursValid(int hours) {
-        // Implement method
-        return false;
+        if (hours > 23 || hours < 0){
+          return false;
+        }
+        else {
+          return true;
+        }
     }
 
-
     public static boolean areMinutesValid(int minutes) {
-        // Implement method
-        return false;
+        if(minutes > 59 || minutes < 0) {
+          return false;
+        }
+        else {
+          return true;
+        }
     }
 }
